98d514e1cd0bfd9f59228209278ba5b5813b4e79
charliedayfockens-hue <charliedayfockens@gmail.com>
2026-03-06 22:08:29 -0600

    aa

--- Morevane VR/Assets/Scripts/Playfablogin.cs
+++ Morevane VR/Assets/Scripts/Playfablogin.cs
@@ -114,7 +114,7 @@ public class Playfablogin : MonoBehaviour
 
     void OnGetUserInventorySuccess(GetUserInventoryResult result)
     {
-        coins = result.VirtualCurrency["HS"];
+        coins = result.VirtualCurrency["MV"];
         currencyText.text = "You have " + coins.ToString() + " " + CurrencyName;
     }
 
