d282e907d0486cfe2aba697faa40ff5838ba16dd
elirsmey-alt <elirsmey@gmail.com>
2026-03-16 20:21:12 -0400

    asd

--- Morevane VR/Assets/Resources/PhotonVR/Scripts/Player/PhotonVRPlayer.cs
+++ Morevane VR/Assets/Resources/PhotonVR/Scripts/Player/PhotonVRPlayer.cs
@@ -18,7 +18,7 @@ namespace Photon.VR.Player
         public Transform LeftHand;
         public Transform RightHand;
         [Tooltip("The objects that will get the colour of the player applied to them")]
-        public List<SkinnedMeshRenderer> ColourObjects;
+        public List<MeshRenderer> ColourObjects;
 
         [Header("Cosmetics Parents")]
         public Transform HeadCosmetics;
@@ -82,7 +82,7 @@ namespace Photon.VR.Player
                 NameText.text = photonView.Owner.NickName;
 
             // Colour
-            foreach (SkinnedMeshRenderer renderer in ColourObjects)
+            foreach (MeshRenderer renderer in ColourObjects)
             {
                 if(renderer != null)
                     renderer.material.color = JsonUtility.FromJson<Color>((string)photonView.Owner.CustomProperties["Colour"]);
